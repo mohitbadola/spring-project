943e47733d8a026d4869f3690ab6f96e040d4bbf
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-27 01:12:46 +0530

    Setter Injection

--- src/main/java/com/jason/App.java
+++ src/main/java/com/jason/App.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ * App
  *
  */
 public class App 
