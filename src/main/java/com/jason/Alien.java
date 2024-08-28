0ccbb3bdebdf634e4b75c67caab851537f8f4511
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-08-28 19:56:53 +0530

    Make Desktop Scope prototype and use value annotation

--- src/main/java/com/jason/Alien.java
+++ src/main/java/com/jason/Alien.java
@@ -2,10 +2,13 @@ package com.jason;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+
+    @Value("21")
     private int age;
 
 //    @Autowired
--- src/main/java/com/jason/Desktop.java
+++ src/main/java/com/jason/Desktop.java
@@ -1,10 +1,12 @@
 package com.jason;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("comp2")
 @Primary                    //Qualifier have more preference than Primary
+@Scope("prototype")
 public class Desktop implements Computer{
     public Desktop() {
         System.out.println("Desktop object Created");
